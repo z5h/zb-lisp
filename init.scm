d8abb9d93f203bae08123cc60daac410a937123e
Mark Bolusmjak <mark@mark-mac-pro.local>
2010-03-03 17:29:30 -0500

    fixes

--- init.scm
+++ init.scm
@@ -4,6 +4,29 @@
 			(cons (f (car l)) 
 						(map f (cdr l))))))
 
+(set! foldr 
+	(lambda (func end lst)
+		(if (null? lst)  
+			end
+			(func (car lst) (foldr func end (cdr lst))))))
+
+(set! foldl 
+	(lambda (func accum lst)
+	(if (null? lst)
+		accum
+			(foldl func (func accum (car lst)) (cdr lst)))))
+
+(set! fold foldl)
+(set! reduce fold)
+
+(set! filter 
+	(lambda (pred lst)   
+	(foldr 
+		(lambda (x y) 
+				(if (pred x) 
+					(cons x y) y)) 
+			'() lst)))
+
 (set! not 
 	(lambda (x)
 		(if x #f #t)))
